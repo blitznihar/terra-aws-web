93ba6521ab5363930832f19b711bcf6c60e651a6
root <root@DESKTOP-21J9S8I.localdomain>
2019-02-16 18:57:34 -0600

    Added network and basic compute

---
+++ compute/variable.tf
@@ -0,0 +1,6 @@
+
+variable "aws_subnet_subnet_01_devops_id" { }
+
+variable "aws_subnet_subnet_02_devops_id" { }
+
+variable "aws_security_group_aws_security_group_devops_id"{ }
--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "env" {
   description = "env: dev or prod"
 }
 
-#Create S3 Bucket in AWS
+
 variable "aws_access_key" {
     description = "For aws_access_key"
     type="map"
@@ -13,12 +13,55 @@ variable "aws_secret_key" {
     type="map"
 }
 
+variable "region" {
+    description = "For AWS Region"
+    type="map"
+}
+
+#Create S3 Bucket in AWS
 variable "project_name" {
     description = "For project Name"
     type="map"
 }
 
-variable "region" {
-    description = "For AWS Region"
+
+
+# Network Variables
+variable "cidr_block" {
+    description = "To define CIDR block for the VPC connection"
+    type="map" 
+}
+
+variable "cidr_subnet1_block" {
+    description = "To define CIDR block for Subnet A"
+    type="map"
+}
+
+variable "cidr_subnet2_block" {
+    description = "To define CIDR block for Subnet B"
+    type="map"
+}
+
+variable "cidr_subnet3_block" {
+    description = "To define CIDR block for Subnet C"
+    type="map"
+}
+
+variable "cidr_subnet1_availability_zone" {
+    description = "To define availability zone for Subnet A"
+    type="map"
+}
+variable "cidr_subnet2_availability_zone" {
+    description = "To define availability zone for Subnet B"
+    type="map"
+}
+
+variable "cidr_subnet3_availability_zone" {
+    description = "To define availability zone for Subnet C"
+    type="map"  
+}
+
+variable "access_ip" {
+    description = "To define ip address to be able to access on ssh"
     type="map"
 }
